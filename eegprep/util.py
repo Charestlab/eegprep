c7f67b8a11340212b27631f404cce91d00c14d91
Jasper <vandejjf@bham.ac.uk>
2018-12-03 15:02:30 +0000

    added blank line eof to utils

--- eegprep/util.py
+++ eegprep/util.py
@@ -9,3 +9,5 @@ def resample_events_on_resampled_epochs(epochs, orig_sfreq):
     events = epochs.events.copy()
     events[:, 0] = numpy.round(events[:, 0] * ratio).astype(int)
     epochs.events = events
+  
+
