03e374604b213fcfc337fd2138f80915500f58f4
Jasper van den Bosch <japsai@gmail.com>
2019-11-22 23:51:03 +0000

    notes on i/o approach

--- eegprep/main.py
+++ eegprep/main.py
@@ -1,5 +1,32 @@
 from eegprep.args import parse_arguments
+from os.path import join, basename, splitext
+import os, glob, random
+from eegprep.preproc_subject import preproc_subject
+from bids import BIDSLayout
+
 
 def run():
     args = parse_arguments()
-    print(args)
+
+
+    
+    layout = BIDSLayout(args.data_directory)
+    subjects = layout.get(return_type='id', target='subject')
+
+
+    # IO
+    # contains input, output, mem_storage, report
+    # job = subject(io.for(subject=sub))
+    #   io.store(for_run=run, epochs) # io decides whether to keep in memory or to store temp or store long term
+    # pipeline.add(job)
+    # pipeline.run()
+
+
+
+    preproc_subject(layout, subject)
+
+    # output
+    eegprepdir = join(args.data_directory, 'derivatives', 'eegprep')
+    subjectdir = join(eegprepdir, 'sub-' + subject)
+    os.makedirs(subjectdir, exist_ok=True)
+    out_fpath = join(subjectdir, 'sub-{}_epo.npz'.format(subject))
